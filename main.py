6711e1948908a2ac7c6e7a5f6ac141e66d926d20
dush-ik <dushik@live.com>
2024-11-17 16:12:38 +0530

    feat: lcs

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-def longest_common_subsequence():
-    # ToDo 
+def handle_index(lcs, x_index, y_index):
+  if x_index < 0 or y_index < 0:
+    return 0
+  return lcs[x_index][y_index]    
+
+def longest_common_subsequence (x, y):
+  x_size = len(x)
+  y_size = len(y)
+  lcs = [[0] * (y_size) for _ in range(x_size)]
+  str = ''
+  for i in range(x_size):
+    for j in range(y_size):
+      if x[i] == y[j]:
+        lcs[i][j] = handle_index(lcs, i-1, j-1) + 1
+        str += x[i]
+      else:
+        lcs[i][j] = max(handle_index(lcs, i-1, j-1),
+            handle_index(lcs, i, j-1),
+            handle_index(lcs, i-1, j))
+
+  return (lcs[x_size-1][y_size-1], str)
+
+
